e12c27747d96b46cf76024767a3db9e1a308d641
Søren Sjørup <soren@zoren.dk>
2023-12-06 12:55:36 +0100

    part2

--- 6/index.js
+++ 6/index.js
@@ -1,5 +1,5 @@
 let f = (t, ht) => (t - ht) * ht;
-let prod = 1
+let prod = 1;
 const example = [
   [7, 9],
   [15, 40],
@@ -8,19 +8,21 @@ const example = [
 // Time:        49     97     94     94
 // Distance:   263   1532   1378   1851
 const input = [
- [49, 263],
+  [49, 263],
   [97, 1532],
   [94, 1378],
   [94, 1851],
-]
-for (const [t, dist] of input) {
-  console.log({ t, dist})
-  let winnings = 0
+];
+
+const part2example = [[71530, 940200]];
+const part2input = [[49979494, 263153213781851]];
+for (const [t, dist] of part2input) {
+  console.log({ t, dist });
+  let winnings = 0;
   for (let i = 0; i < t; i++) {
     const travelled = f(t, i);
-    console.log({ holdTime: i, travelled, won: travelled > dist });
+    // console.log({ holdTime: i, travelled, won: travelled > dist });
     if (travelled > dist) {
-      // break;
       winnings++;
     }
   }
