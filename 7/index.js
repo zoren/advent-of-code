514a7826193178d803823c1162efa029dfc31e7d
Søren Sjørup <soren@zoren.dk>
2023-12-07 21:40:54 +0100

    p1

---
+++ 7/index.js
@@ -0,0 +1,54 @@
+const cards = "AKQJT98765432";
+
+const type = (hand) => {
+  const cards = hand.split("");
+  const hist = {};
+  for (const c of cards) {
+    hist[c] = (hist[c] || 0) + 1;
+  }
+  if (cards.every((c) => c === cards[0]))
+    return { name: "Five of a kind", rank: 0 };
+  const values = Object.values(hist).sort().reverse();
+  if (values[0] === 4) return { name: "Four of a kind", rank: 1 };
+  if (values[0] === 3 && values[1] === 2)
+    return { name: "Full house", rank: 2 };
+  if (values[0] === 3) return { name: "Three of a kind", rank: 3 };
+  if (values[0] === 2 && values[1] === 2) return { name: "Two pair", rank: 4 };
+  if (values[0] === 2) return { name: "Pair", rank: 5 };
+  if (values.every((v) => v === 1)) return { name: "High card", rank: 6 };
+  throw new Error("Should not happen");
+};
+
+const compareHands = (a, b) => {
+  const aType = type(a);
+  const bType = type(b);
+  if (aType.rank !== bType.rank) return aType.rank - bType.rank;
+  const aCards = a.split("").map((c) => cards.indexOf(c));
+  const bCards = b.split("").map((c) => cards.indexOf(c));
+  for (let i = 0; i < aCards.length; i++) {
+    if (aCards[i] !== bCards[i]) return aCards[i] - bCards[i];
+  }
+  return 0;
+}
+
+const example = `
+32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483
+`;
+import text from "./input?raw";
+const lines = text
+  .trim()
+  .split("\n")
+  .filter((s) => s !== "");
+const hands = lines.map((line) => {
+  const [hand, score] = line.split(" ");
+  return { hand, type: type(hand).name, score: parseInt(score) };
+});
+hands.sort((a, b) => compareHands(a.hand, b.hand));
+console.log(hands);
+const count = hands.length
+const res = hands.reduce((acc, { score }, i) => acc + score * (count - i), 0);
+console.log(res);
